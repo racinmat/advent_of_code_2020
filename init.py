24596f1a7f68fa4a935cf80a5a510fb0091739ae
Matěj Račinský <racinsky.matej@seznam.cz>
2020-12-02 13:06:39 +0100

    using modules for code generation

--- init.py
+++ init.py
@@ -1,10 +1,13 @@
 import os
 import os.path as osp
 for i in range(1, 26):
-    os.makedirs(f'day_{i}', exist_ok=True)
-    open(f'day_{i}/input.txt', 'w+').close()
-    with open(f'day_{i}/main.jl', 'w+') as f:
-        f.write("""using DrWatson
+    os.makedirs(f'day_{i:02d}', exist_ok=True)
+    open(f'day_{i:02d}/input.txt', 'w+').close()
+    with open(f'day_{i:02d}/main.jl', 'w+') as f:
+        f.write(f"""
+module Day:{i:02d}
+
+using DrWatson
 quickactivate(@__DIR__)
 include(projectdir("misc.jl"))
 
@@ -19,8 +22,10 @@ function part2()
     data
 end
 
+if false
 println(part1())
 submit(part1(), cur_day, 1)
 println(part2())
 submit(part2(), cur_day, 2)
+end
 """)
