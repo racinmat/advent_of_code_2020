abd5ff78ff23217caca16e4a4161209fb889b685
Matěj Račinský <matej.racinsky@avast.com>
2020-12-06 12:38:37 +0100

    added day 6

--- day_06/main.jl
+++ day_06/main.jl
@@ -6,14 +6,16 @@ include(projectdir("misc.jl"))
 
 const cur_day = parse(Int, splitdir(@__DIR__)[end][5:end])
 const raw_data = cur_day |> read_input
-process_data() = raw_data
+process_data() = raw_data |> x->read_lines(x, "\n\n") .|> x->split(x, "\n")
 
 function part1()
     data = process_data()
+    (data .|> collect .|> x->union(x...)) .|> length |> sum
 end
 
 function part2()
     data = process_data()
+    (data .|> collect .|> x->intersect(x...)) .|> length |> sum
 end
 
 
