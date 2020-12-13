8ca797f53460c99a7efa09577c47e2b951d5e2f9
Matěj Račinský <matej.racinsky@avast.com>
2020-12-13 18:17:14 +0100

    added more test inputs

--- day_13/main.jl
+++ day_13/main.jl
@@ -12,7 +12,10 @@ end
 const cur_day = parse(Int, splitdir(@__DIR__)[end][5:end])
 const raw_data = cur_day |> read_input
 process_data() = raw_data |> read_lines |> x->(parse(Int, x[1]), parse_buses(x))
-test_data =  cur_day |> read_file("test_input.txt") |> read_lines |> x->(parse(Int, x[1]), parse_buses(x))
+test_data = cur_day |> read_file("test_input.txt") |> read_lines |> x->(parse(Int, x[1]), parse_buses(x))
+test_data = cur_day |> read_file("test_input_2.txt") |> read_lines |> x->(parse(Int, x[1]), parse_buses(x))
+test_data = cur_day |> read_file("test_input_3.txt") |> read_lines |> x->(parse(Int, x[1]), parse_buses(x))
+test_data = cur_day |> read_file("test_input_4.txt") |> read_lines |> x->(parse(Int, x[1]), parse_buses(x))
 
 function part1()
     data = process_data()
@@ -52,7 +55,7 @@ function combine_buses(ok_buses)
     for i in sorted_idxs[2:end]
         period_combined, phase_combined = combine_buses(period_combined, ok_buses[i], phase_combined, i)
     end
-
+    period_combined, phase_combined
 end
 
 function part2()
@@ -61,38 +64,27 @@ function part2()
     time_start, buses = data
     ok_buses = Dict(k-1=>v for (k, v) in enumerate(buses) if !isnothing(v))
     # sorted_idxs = sort(collect(keys(ok_buses)))
-    # bus1 = ok_buses[sorted_idxs[1]]
-    # bus2 = ok_buses[sorted_idxs[2]]
-    # period_12, phase_12 = combine_buses(bus1, bus2, sorted_idxs[1], sorted_idxs[2])
-    # time_12_fit = find_pair_meets(period_12, phase_12)
-    # bus3 = ok_buses[sorted_idxs[3]]
-    # period_123, phase_123 = combine_buses(period_12, bus3, phase_12, sorted_idxs[3])
-    # time_123_fit = find_pair_meets(period_123, phase_123)
-    # bus4 = ok_buses[sorted_idxs[4]]
-    # period_1234, phase_1234 = combine_buses(period_123, bus4, phase_123, sorted_idxs[4])
-    # time_1234_fit = find_pair_meets(period_1234, phase_1234)
-    # bus5 = ok_buses[sorted_idxs[5]]
-    # period_12345, phase_12345 = combine_buses(period_1234, bus5, phase_1234, sorted_idxs[5])
-    # time_12345_fit = find_pair_meets(period_12345, phase_12345)
-    #
+    # period_combined, phase_combined = combine_buses(ok_buses)
+    period_combined, phase_combined = combine_buses(filter(kv->kv[1]<50, ok_buses))
+    group_meets = find_pair_meets(period_combined, phase_combined)
     # # x = ok_buses[sorted_idxs[1]]
     # # x = time_12_fit
     # # x = time_123_fit
     # x = time_1234_fit
-    # next_log = 0
-    # while buses_not_fit(x, ok_buses)
-    #     # x += ok_buses[0]
-    #     # x += period_12
-    #     # x += period_123
-    #     x += period_1234
-    #     # if x > next_log
-    #     #     next_log += 1_000_000_000
-    #     #     @info Dates.now() x
-    #     # end
-    # end
-    # x
-    period_combined, phase_combined = combine_buses(ok_buses)
-    find_pair_meets(period_combined, phase_combined)
+    x = group_meets
+    next_log = 0
+    while buses_not_fit(x, ok_buses)
+        # x += ok_buses[0]
+        # x += period_12
+        # x += period_123
+        # x += period_1234
+        x += period_combined
+        if x > next_log
+            next_log += 1_000_000_000
+            @info Dates.now() x
+        end
+    end
+    x
 end
 
 # todo: figure it out, try on more test inputs, combine iterative approach and this one
@@ -100,6 +92,8 @@ end
 # That's not the right answer; your answer is too high.  If you're stuck, make sure you're using the full input data; there are also some general tips on the about page, or you can ask for hints on the subreddit.  Please wait one minute before trying
 # again. (You guessed 578230687890668.) [Return to Day 13]
 # 289115343945334 is too low
+# 289_115_343_945_334
+# 578_230_687_890_668
 end # module
 
 if false
