71145d601e92d84a60448b58c1bd9f888836f910
Matěj Račinský <matej.racinsky@avast.com>
2020-12-13 15:34:27 +0100

    done day 13 part 1

--- day_13/main.jl
+++ day_13/main.jl
@@ -4,16 +4,39 @@ using DrWatson
 quickactivate(@__DIR__)
 include(projectdir("misc.jl"))
 
+using Dates
+function parse_buses(x)
+    tryparse.(Int, split(x[2], ","))
+end
+
 const cur_day = parse(Int, splitdir(@__DIR__)[end][5:end])
 const raw_data = cur_day |> read_input
-process_data() = raw_data
+process_data() = raw_data |> read_lines |> x->(parse(Int, x[1]), parse_buses(x))
+test_data =  cur_day |> read_file("test_input.txt") |> read_lines |> x->(parse(Int, x[1]), parse_buses(x))
 
 function part1()
     data = process_data()
+    # data = test_data
+    time_start, buses = data
+    buses = filter(!isnothing, buses)
+    times2arrive = buses .- (time_start .% buses)
+    idx = argmin(times2arrive)
+    times2arrive[idx] * buses[idx]
 end
+
+buses_not_fit(x, ok_buses) = any((x+k) % v != 0 for (k, v) in ok_buses)
 
 function part2()
     data = process_data()
+    # data = test_data
+    time_start, buses = data
+    ok_buses = Dict(k-1=>v for (k, v) in enumerate(buses) if !isnothing(v))
+    x = ok_buses[0]
+    while buses_not_fit(x, ok_buses)
+        x += ok_buses[0]
+    end
+
+    x
 end
 
 
