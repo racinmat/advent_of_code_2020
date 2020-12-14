2114da33211746d458b1281eb573cd2c65962465
Matěj Račinský <racinsky.matej@seznam.cz>
2020-12-14 22:29:25 +0100

    done day 14 part 1

--- day_14/main.jl
+++ day_14/main.jl
@@ -4,16 +4,78 @@ using DrWatson
 quickactivate(@__DIR__)
 include(projectdir("misc.jl"))
 
+const mapping = Dict('0'=>false, '1'=>true, 'X'=>missing)
+
+function parse_row(x)
+    if startswith(x, "mask ")
+        m = match(r"mask = ((?:\d|X)+)", x)
+        return "mask", getindex.(Ref(mapping), collect(m[1]))
+    else
+        m = match(r"mem\[(\d+)\] = (\d+)", x)
+        return "mem", (parse(Int, m[1]), parse(Int, m[2]))
+    end
+    tryparse.(Int, split(x[2], ","))
+end
+
 const cur_day = parse(Int, splitdir(@__DIR__)[end][5:end])
 const raw_data = cur_day |> read_input
-process_data() = raw_data
+process_data() = raw_data |> read_lines .|> parse_row
+test_data = cur_day |> read_file("test_input.txt") |> read_lines .|> parse_row
+test_data = cur_day |> read_file("test_input_2.txt") |> read_lines .|> parse_row
+
+dec2bin(x) = parse.(Bool, collect(bitstring(x)))[end-35:end]
+merge_with_mask(x, mask) = ismissing(mask) ? x : mask
+bin2dec(x) = Int(reinterpret(UInt64, reverse(x).chunks)[1])
+
+merge_with_mask2(x, mask) = isequal(mask, 0) ? x : mask
 
 function part1()
     data = process_data()
+    # data = test_data
+    memory = Dict{Int, Int}()
+    cur_mask = nothing
+    for (i, j) in data
+        if i == "mask"
+            cur_mask = j
+        elseif i == "mem"
+            idx, val = j
+            memory[idx] = bin2dec(merge_with_mask.(dec2bin(val), cur_mask))
+        end
+    end
+    sum(values(memory))
 end
+
+sum_floats(pos, floatings) = sum(p*2^(f-1) for (p, f) in zip(pos, floatings))
 
 function part2()
     data = process_data()
+    data = test_data
+    memory = Dict{Int, Vector}()
+    cur_mask = nothing
+    # todo: rewrite it all, I must write same thing to multiple places, not summing them
+    i, j = data[1]
+    i, j = data[2]
+    for (i, j) in data
+        if i == "mask"
+            cur_mask = j
+        elseif i == "mem"
+            idx, val = j
+            # todo: fix
+            memory[idx] = merge_with_mask2.(dec2bin(val), cur_mask)
+        end
+    end
+    total = 0
+    i = collect(values(memory))[2]
+    for i in values(memory)
+        floatings = findall(ismissing, reverse(i))
+        pos = collect(Iterators.product(fill(0:1,length(floatings))...))[:][4]
+        base_num = sum(2 .^ (findall(isequal(true), reverse(i)).-1))
+        all_combs = collect(Iterators.product(fill(0:1,length(floatings))...))[:]
+        sum(sum_floats(pos, floatings) for pos in all_combs) + length(all_combs) * base_num
+
+        end
+    end
+    sum(values(memory))
 end
 
 
