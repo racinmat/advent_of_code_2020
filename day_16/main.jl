70017bd0a51a060fdbca2578a93cbf226fcd1847
Matěj Račinský <matej.racinsky@avast.com>
2020-12-16 19:20:20 +0100

    done day 16 part 2

--- day_16/main.jl
+++ day_16/main.jl
@@ -23,6 +23,7 @@ const cur_day = parse(Int, splitdir(@__DIR__)[end][5:end])
 const raw_data = cur_day |> read_input
 process_data() = raw_data |> x->split(x, "\n\n") |> x->parse_input(x...)
 test_data = cur_day |>read_file("test_input.txt") |> x->split(x, "\n\n") |> x->parse_input(x...)
+test_data = cur_day |>read_file("test_input_2.txt") |> x->split(x, "\n\n") |> x->parse_input(x...)
 
 function part1()
     data = process_data()
@@ -34,12 +35,23 @@ end
 
 function part2()
     data = process_data()
-    data = test_data
+    # data = test_data
     conditions, my_ticket_nums, other_tickets_nums = data
-    all_nums = other_tickets_nums
-    valid_tickets = filter(j->all(i->any(c->any(i .∈ c[2:3]), conditions), j), other_tickets_nums)
-    filter(i->!any(c->any(i .∈ c[2:3]), conditions), other_tickets_nums)
-    filter(i->!any(c->any(i .∈ c[2:3]), conditions), other_tickets_nums)
+    valid_tickets = hcat(my_ticket_nums, filter(j->all(i->any(c->any(i .∈ c[2:3]), conditions), j), other_tickets_nums)...)'
+    valid_mappings = Dict(i=>findall(cond->all(i->any(i .∈ cond[2:3]), col), conditions) for (i, col) in enumerate(eachcol(valid_tickets)))
+    valid_mappings
+    true_mapping = Dict{Int, Int}()
+    while length(true_mapping) < length(valid_mappings)
+        certain = Dict(first(v)=>k for (k, v) in valid_mappings if length(v) == 1)
+        merge!(true_mapping, certain)
+        vals2remove = keys(certain)
+        foreach(kv->deleteat!(kv[2], findall(x->x ∈ vals2remove,kv[2])), valid_mappings)
+    end
+    true_mapping
+    col_names = Dict(name=>true_mapping[i] for  (i, name) in enumerate(getindex.(conditions, 1)))
+    departure_fields = filter(x->startswith(x, "departure"), keys(col_names))
+    departure_idxs = getindex.(Ref(col_names), departure_fields)
+    prod(my_ticket_nums[departure_idxs])
 end
 
 
