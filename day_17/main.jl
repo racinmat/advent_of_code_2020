ea3ab46eb7b285af30c60c21c0793feb41cfd8a2
Matěj Račinský <matej.racinsky@avast.com>
2020-12-17 20:32:20 +0100

    added input day 17

--- day_17/main.jl
+++ day_17/main.jl
@@ -6,7 +6,8 @@ include(projectdir("misc.jl"))
 
 const cur_day = parse(Int, splitdir(@__DIR__)[end][5:end])
 const raw_data = cur_day |> read_input
-process_data() = raw_data
+process_data() = raw_data |> replace_chars("."=>"0", "#"=>"1") |> read_lines .|> collect .|> (x->parse.(Int, x)) |> x->hcat(x...)'
+test_data = cur_day |> read_file("test_input.txt") |> replace_chars("."=>"0", "L"=>"1") |> read_lines .|> collect .|> (x->parse.(Int, x)) |> x->hcat(x...)'
 
 function part1()
     data = process_data()
