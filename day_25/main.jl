0a3ec3302d80eaae082205b622c8e3588c9f74ad
Matěj Račinský <matej.racinsky@avast.com>
2020-12-27 23:31:22 +0100

    day 25 part 1 done

--- day_25/main.jl
+++ day_25/main.jl
@@ -6,10 +6,37 @@ include(projectdir("misc.jl"))
 
 const cur_day = parse(Int, splitdir(@__DIR__)[end][5:end])
 const raw_data = cur_day |> read_input
-process_data() = raw_data
+process_data() = raw_data |> read_lines .|> x->parse(Int, x)
+test_data = 5764801, 17807724
+
+do_loop(i, j) = (i * j) % 20201227
 
 function part1()
     data = process_data()
+    # data = test_data
+    pub1, pub2 = data
+
+    cur = 1
+    loop1 = 0
+    while cur != pub1
+        cur = do_loop(cur, 7)
+        loop1 += 1
+    end
+
+    cur = 1
+    loop2 = 0
+    while cur != pub2
+        cur = do_loop(cur, 7)
+        loop2 += 1
+    end
+
+    cur = 1
+    loop3 = 0
+    for i in 1:loop1
+        cur = do_loop(cur, pub2)
+    end
+
+    cur
 end
 
 function part2()
