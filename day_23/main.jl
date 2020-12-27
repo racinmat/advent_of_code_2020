1fd71c402c479c5ac7200385b1969c46e525090c
Matěj Račinský <matej.racinsky@avast.com>
2020-12-27 21:42:05 +0100

    day 23 done

--- day_23/main.jl
+++ day_23/main.jl
@@ -13,65 +13,131 @@ test_data = "389125467" |> collect .|> x->parse(Int, x)
 using TimerOutputs
 const to = TimerOutput()
 
-function do_step!(q, cur_idx)
+function do_step!(q::Vector, cur_idx)
     @inbounds cur = q[cur_idx]
-    @timeit to "pop1" r1 = popat!(q, cur_idx+1 > length(q) ? 1 : cur_idx+1)
-    @timeit to "pop2" r2 = popat!(q, cur_idx+1 > length(q) ? 1 : cur_idx+1)
-    @timeit to "pop3" r3 = popat!(q, cur_idx+1 > length(q) ? 1 : cur_idx+1)
+    #=@timeit to "pop1"=# r1 = popat!(q, mod1(cur_idx+1, length(q)))
+    #=@timeit to "pop2"=# r2 = popat!(q, mod1(cur_idx+1, length(q)))
+    #=@timeit to "pop3"=# r3 = popat!(q, mod1(cur_idx+1, length(q)))
+
+    dest_val = mod1(cur - 1, length(q))
+    while dest_val == r1 || dest_val == r2 || dest_val == r3
+        dest_val = mod1(dest_val - 1, length(q))
+    end
+    #=@timeit to "find dest"=# dest_idx = findfirst(==(dest_val), q)
+    insert!(q, dest_idx+1, r1)
+    insert!(q, dest_idx+2, r2)
+    insert!(q, dest_idx+3, r3)
+    #=@timeit to "find cur"=# cur_idx = findfirst(==(cur), q)
+    cur_idx = cur_idx + 1 > length(q) ? 1 : cur_idx + 1
+    cur_idx
+end
+
+function do_step!(q::MutableLinkedList)
+    cur = first(q)
+    rotate_left!(q)
+
+    #=@timeit to "pop1"=# r1 = popfirst!(q)
+    #=@timeit to "pop2"=# r2 = popfirst!(q)
+    #=@timeit to "pop3"=# r3 = popfirst!(q)
 
     dest_val = cur - 1 == 0 ? maximum(q) : cur - 1
     while dest_val == r1 || dest_val == r2 || dest_val == r3
         dest_val = dest_val - 1 == 0 ? maximum(q) : dest_val - 1
     end
-    @timeit to "find dest" dest_idx = findfirst(==(dest_val), q)
-    insert!(q, dest_idx+1, r1)
-    insert!(q, dest_idx+2, r2)
-    insert!(q, dest_idx+3, r3)
-    @timeit to "find cur" cur_idx = findfirst(==(cur), q)
-    cur_idx = cur_idx + 1 > length(q) ? 1 : cur_idx + 1
-    cur_idx
+    #=@timeit to "find dest"=# rotate_left_until!(q, ==(dest_val))
+    rotate_left!(q)
+    pushfirst!(q, r3)
+    pushfirst!(q, r2)
+    pushfirst!(q, r1)
+    #=@timeit to "find cur"=# rotate_left_until!(q, ==(cur))
+    rotate_left!(q)
+end
+
+mutable struct StaticLinkedList
+    next::Vector{Int}
+    head::Int
+    tail::Int
+
+    StaticLinkedList(i::Int) = new(zeros(Int, i), 0, 0)
+    StaticLinkedList(i::Int, j::Int) = new(zeros(Int, i), j, 0)
+    StaticLinkedList(i::Int, j::Int, k::Int) = new(zeros(Int, i), j, k)
+end
+
+import Base: length, first, iterate
+length(q::StaticLinkedList) = length(q.next)
+first(q::StaticLinkedList) = q.head
+
+iterate(q::StaticLinkedList) = length(q) == 0 ? nothing : (q.head, q.next[q.head])
+iterate(q::StaticLinkedList, i::Int) = i == 0 ? nothing : (i, i == q.tail ? 0 : q.next[i])
+
+function do_step!(q::StaticLinkedList)
+    cur = first(q)
+
+    r1 = q.next[cur]
+    r2 = q.next[r1]
+    r3 = q.next[r2]
+    new_next = q.next[r3]
+    q.next[cur] = new_next
+
+    dest_val = mod1(cur - 1, length(q))
+    while dest_val == r1 || dest_val == r2 || dest_val == r3
+        dest_val = mod1(dest_val - 1, length(q))
+    end
+
+    # todo: ošetřit u každého nextu, že to bude nula, a místo toho vzít head
+    new_next = q.next[dest_val]
+    q.next[dest_val] = r1
+    q.next[r3] = new_next
+
+    q.head = q.next[cur]
+    # q.next[q.tail] = cur
+    q.tail = cur
+    # q.next[q.tail] = 0
+end
+
+function build_static_linked_list(data)
+    q = StaticLinkedList(length(data), first(data), last(data))
+
+    for i in eachindex(data)
+        if i == length(data)
+            q.next[data[i]] = q.head
+        else
+            q.next[data[i]] = data[i+1]
+        end
+    end
+    q
 end
 
 function part1()
     data = process_data()
     # data = test_data
-    # q = CircularDeque{Int}(length(data))
-    # for d in data
-    #     push!(q, d)
-    # end
-    # q = CircularBuffer{Int}(length(data))
-    q = copy(data)
-    cur_idx = 1
+    q = build_static_linked_list(data)
 
     for i in 1:100
-        cur_idx = do_step!(q, cur_idx)
+        do_step!(q)
     end
 
-    q
-    idx1 = findfirst(==(1), q)
-    vcat(q[idx1+1:end], q[begin:idx1-1]) .|> string |> x->reduce(*, x)
+    q_vec = collect(q)
+    idx1 = findfirst(==(1), q_vec)
+    vcat(q_vec[idx1+1:end], q_vec[begin:idx1-1]) .|> string |> x->reduce(*, x)
 end
 
 function part2()
     data = process_data()
     # data = test_data
-    # q = CircularDeque{Int}(length(data))
-    # for d in data
-    #     push!(q, d)
-    # end
-    # q = CircularBuffer{Int}(length(data))
-    q = vcat(copy(data),(length(data)+1):1_000)
-    # q = vcat(copy(data),(length(data)+1):1_000_000)
-    cur_idx = 1
+    # q = build_static_linked_list(vcat(copy(data),(length(data)+1):1_000))
+    q = build_static_linked_list(vcat(copy(data),(length(data)+1):1_000_000))
 
-    # for i in 1:10_000_000
-    for i in 1:10_000
-        @timeit to "do_step!" cur_idx = do_step!(q, cur_idx)
+    for i in 1:10_000_000
+    # for i in 1:10_000
+        do_step!(q)
     end
 
-    q
-    idx1 = findfirst(==(1), q)
-    vcat(q[idx1+1:end], q[begin:idx1-1]) .|> string |> x->reduce(*, x)
+    q_vec = collect(q)
+    idx1 = findfirst(==(1), q_vec)
+    next_1 = q.next[1]
+    next_next_1 = q.next[next_1]
+    next_1 * next_next_1
 end
 
 
@@ -87,6 +153,7 @@ Day23.submit(Day23.part2(), Day23.cur_day, 2)
 using BenchmarkTools
 @benchmark Day23.part1()
 @btime Day23.part2()
+@btime Day23.part2_linked()
 
 Day23.reset_timer!(Day23.to)
 Day23.part2()
